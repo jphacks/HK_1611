ac7f1f36328b94cc29a1740d1262426196e13d11
会津慎弥 <b1013043@fun.ac.jp>
2016-11-05 10:21:39 +0900

    add position sensor

--- tenorail/tenorail.ino
+++ tenorail/tenorail.ino
@@ -1,9 +1,19 @@
+#define SENSORPIN  (0)
+#define VALUE_1   (0)
+#define POSITION_1  (0.0)
+#define VALUE_2   (1023)
+#define POSITION_2  (20.0)
+
 void setup() {
   // put your setup code here, to run once:
-
+ Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+   int positionValue = analogRead( SENSORPIN );
+   int dPosition = map(positionValue,VALUE_1,VALUE_2,POSITION_1,POSITION_2); 
+  Serial.println(dPosition);
 
 }
